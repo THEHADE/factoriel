e2a4bc850d05029378489369df38ea6386760b0a
uvhvy <uvhvy@DESKTOP-OM746DK>
2023-11-01 14:04:53 +0330

    Add project files.

---
+++ factoriel/Program.cs
@@ -0,0 +1,27 @@
+﻿//amir hossein hemmati
+//T.3 factoriel
+
+
+int adad;
+string yesorno;
+
+do
+{
+    Console.WriteLine("adad khood ra baraye anjam mohasebat vared konid:");
+    adad = Convert.ToInt32(Console.ReadLine());
+
+    long x = adad;
+
+
+    if (adad == 0) x = 1;
+    for (int i = 1; i < adad; i++)
+
+    {
+        x = x * i;
+    }
+    Console.WriteLine(x);
+
+    Console.WriteLine("aya mikhahid edame dahid? (bale/kheir)");
+    yesorno = Console.ReadLine();
+
+} while (Console.ReadLine() == "bale");
